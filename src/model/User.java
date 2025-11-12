a6d6eabbd72165e249b68fbc16c11a899d16e6da
IK08R <i.katalanina@gmail.com>
2025-11-12 21:15:28 +0700

    Fix User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,20 +1,31 @@
+/**
+ * версия модели пользователя.
+ * 
+ * Пока что можно:
+ * - Просто хранит логин, пароль и роль.
+ * - Роль задаётся строкой.
+ * 
+ * в перспективе:
+ * - Добавить ID пользователя.
+ * - Реализовать безопасное хранение пароля типо хэширование.
+ */
+
 package model;
 
 public class User {
     private String login;
     private String password;
-    private String role; // "student", "headman", "admin"
+    private String role; // студент, староста, админ
 
     public User(String login, String password, String role) {
         this.login = login;
         this.password = password;
         this.role = role;
     }
-
-    // Геттеры
+    
     public String getLogin() { return login; }
     public String getPassword() { return password; }
     public String getRole() { return role; }
 
-    // TODO: добавить equals(), hashCode(), toString() позже
+    
 }
