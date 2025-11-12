8d0e66c3b9080989070cab98b3229798a705ee4c
IK08R <i.katalanina@gmail.com>
2025-11-12 21:19:36 +0700

    Fix Lesson.java

--- src/model/Lesson.java
+++ src/model/Lesson.java
@@ -1,12 +1,24 @@
+/**
+ * версия занятия (пары).
+ * 
+ * Пока что можно:
+ * - Хранит просто базовые поля.
+ * - Пока нет привязки к пользователю или неделе.
+ * 
+ * в перспективе:
+ * - Добавить WeekType (чётная/нечётная неделя).
+ * - Добавить userId — для персонализации расписания.
+ */
+
 package model;
 
 public class Lesson {
     private String subject;
     private String teacher;
     private String room;
-    private String dayOfWeek; // н-р "Понедельник"
-    private String startTime;  // "09:00"
-    private String endTime;    // "10:30"
+    private String dayOfWeek; // Понедельник
+    private String startTime;  // 09:00
+    private String endTime;    // 10:30
 
     public Lesson(String subject, String teacher, String room, String dayOfWeek, String startTime, String endTime) {
         this.subject = subject;
